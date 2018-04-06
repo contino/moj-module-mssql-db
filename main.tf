224e8e50238db2353a9def69dc4225d57bf0d83b
Chris O'Dell <chris.odell@contino.io>
2018-04-06 11:43:50 +0100

    Tag the resource group wit the environment

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
 resource "azurerm_resource_group" "azurerm_resource_group" {
   name     = "${var.product}-data-${var.env}"
   location = "${var.resource_group_location}"
+
+  tags {
+    environment = "${var.env}"
+  }
 }
 
 resource "random_string" "password" {
@@ -20,9 +24,9 @@ resource "azurerm_sql_firewall_rule" "allow_all_azure_services_rule" {
 }
 
 resource "azurerm_sql_server" "sql_server" {
-  name                = "${var.product}-${var.env}"
-  location            = "${azurerm_resource_group.azurerm_resource_group.location}"
-  resource_group_name = "${azurerm_resource_group.azurerm_resource_group.name}"
+  name                         = "${var.product}-${var.env}"
+  location                     = "${azurerm_resource_group.azurerm_resource_group.location}"
+  resource_group_name          = "${azurerm_resource_group.azurerm_resource_group.name}"
   version                      = "${var.mssql_version}"
   administrator_login          = "${var.mssql_user}"
   administrator_login_password = "${random_string.password.result}"
@@ -34,4 +38,3 @@ resource "azurerm_sql_database" "sql_server_database" {
   resource_group_name = "${azurerm_resource_group.azurerm_resource_group.name}"
   server_name         = "${var.product}-${var.env}"
 }
-
