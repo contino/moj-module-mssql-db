f9b094b8cb551740dda344b5f9c74a8a501b2b79
Janis Berzins <janis.berzins@hmcts.net>
2018-07-25 13:51:12 +0100

    Added new outputs

--- output.tf
+++ output.tf
@@ -11,6 +11,10 @@ output "password" {
 	sensitive = true
 }
 
+output "host_name" {
+	value = "${azurerm_sql_server.sql_server.fully_qualified_domain_name}"
+}
+
 output "database_name" {
 	value = "${azurerm_sql_database.sql_server_database.name}"
 }
@@ -27,4 +31,8 @@ output "jdbc_connection_string" {
 output "odbc_connection_string" {
 	value = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:${azurerm_sql_server.sql_server.fully_qualified_domain_name},1433;Database=${azurerm_sql_server.sql_server.name};Uid=${var.mssql_user}@${azurerm_sql_server.sql_server.name};Pwd=${random_string.password.result};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 	sensitive = true
+}
+
+output "connectionString"{
+	value = "Data Source=${azurerm_sql_server.sql_server.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_sql_server.sql_server.name};Persist Security Info=False;User ID=${var.mssql_user};Password=${random_string.password.result};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
 }
